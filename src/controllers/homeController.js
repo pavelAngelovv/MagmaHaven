dd4483bb12112eaf6a0b1341224e6b4476bf0897
Pavel Angelov <pangelov@codixis.com>
2024-06-22 23:12:43 +0300

    Configure details page functionality

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { getAll, getById } = require("../services/volcanoService");
 
 // TODO replace with real router according to exam description
 
@@ -9,5 +10,25 @@ homeRouter.get('/', async (req, res) => {
  
     res.render('home');
 });
+
+homeRouter.get('/catalog', async (req, res) => {
+    const volcanos = await getAll();
+    console.log(volcanos);
+    res.render('catalog', { volcanos });
+});
+
+homeRouter.get('/catalog/:id', async (req, res) => {
+    const volcano = await getById(req.params.id);
+    if (!volcano) {
+        res.render('404');
+        return;
+    };
+
+    const isOwner = req.user?._id == volcano.owner.toString();
+    // const isLiked = Boolean(stone.likes.find(l => req.user?._id == l.toString()));
+
+    res.render('details', { volcano, isOwner });
+});
+
 
 module.exports = { homeRouter };
